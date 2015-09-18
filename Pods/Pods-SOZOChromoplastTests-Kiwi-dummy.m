c948371891f52c60b589ed31f844059268495c57
Anthony Hoang <anthony@mobilexlabs.com>
2015-09-18 12:02:32 -0500

    commented out the manual allocation. moved the context ref into the scope of 'bitmapDataForImage' so we can release it after it's done

--- Pods/Pods-SOZOChromoplastTests-Kiwi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SOZOChromoplastTests_Kiwi : NSObject
-@end
-@implementation PodsDummy_Pods_SOZOChromoplastTests_Kiwi
-@end
--- SOZOChromoplast/SOZOBitmapDataGenerator.m
+++ SOZOChromoplast/SOZOBitmapDataGenerator.m
@@ -4,7 +4,16 @@
 
 + (NSArray *)bitmapDataForImage:(UIImage *)image {
     NSMutableArray *array = [NSMutableArray new];
-    unsigned char* cArray = [self rawBitmapDataForImage:image];
+//    unsigned char* cArray = [self rawBitmapDataForImage:image];
+    CGContextRef contextRef = [self ARGBContextWithSize:image.size];
+    if (!contextRef) {
+        return NULL;
+    }
+    
+    CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
+    CGContextDrawImage(contextRef, rect, image.CGImage);
+    unsigned char *cArray = CGBitmapContextGetData(contextRef);
+    
     NSInteger numberOfPixels = (NSInteger) [image size].width * [image size].height;
     UIColor *color;
     for (int i = 0; i < numberOfPixels * 4; i += 4) {
@@ -14,6 +23,9 @@
                                 alpha:cArray[i]/255.f];
         [array addObject:color];
     }
+    
+    CGContextRelease(contextRef);
+
     return array;
 }
 
@@ -39,18 +51,18 @@
 
     NSInteger bytesPerPixel = 4;
     unsigned long bytesPerRow = size.width * bytesPerPixel;
-    unsigned long totalBytes = bytesPerRow * size.height;
+//    unsigned long totalBytes = bytesPerRow * size.height;
 
-    void *bitmapData = malloc(totalBytes);
-    if (!bitmapData) {
-        [NSException raise:NSInternalInconsistencyException format:@"Memory allocation error."];
-    }
+//    void *bitmapData = malloc(totalBytes);
+//    if (!bitmapData) {
+//        [NSException raise:NSInternalInconsistencyException format:@"Memory allocation error."];
+//    }
 
     // Cast to silence a type mismatch warning.
     // From the docs: The constants for specifying the alpha channel information are declared
     // with the `CGImageAlphaInfo` type but can be passed to [the bitmapInfo] parameter safely.
     CGBitmapInfo alphaSettings = (CGBitmapInfo) kCGImageAlphaPremultipliedFirst;
-    CGContextRef contextRef = CGBitmapContextCreate(bitmapData, size.width, size.height, 8,
+    CGContextRef contextRef = CGBitmapContextCreate(NULL, size.width, size.height, 8,
                                                     bytesPerRow, colorSpace, alphaSettings);
     if (!contextRef) {
         [NSException raise:NSInternalInconsistencyException format:@"Error creating context."];
